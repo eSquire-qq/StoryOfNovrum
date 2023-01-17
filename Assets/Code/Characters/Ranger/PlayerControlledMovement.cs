bb4afc0eff58d0b75f1b192c18ae62f148ee161a
timur.ptashchenko <timur.ptashchenko@igt.com>
2023-01-17 19:45:35 +0200

    Added basic object interaction

---
+++ Assets/Code/Characters/Ranger/PlayerControlledMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerControlledMovement : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+
+	public Rigidbody2D rb;
+	public Animator animator;
+	public GameObject InteractionArea;
+
+	protected Vector2 movment;
+
+	protected void Update()
+	{
+		movment.x  = Input.GetAxisRaw("Horizontal");
+		movment.y  = Input.GetAxisRaw("Vertical");
+
+		animator.SetFloat("Horizontal", movment.x);
+		animator.SetFloat("Vertical", movment.y);
+		animator.SetFloat("Speed", movment.sqrMagnitude);
+	}
+
+	protected void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + movment * moveSpeed * Time.fixedDeltaTime);
+		InteractionArea.transform.localPosition = movment/2;
+	}
+}
