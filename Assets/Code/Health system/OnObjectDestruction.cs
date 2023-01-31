2ca2ec4d540ef881add0a53aede62d2a20a7e81d
timur.ptashchenko <timur.ptashchenko@igt.com>
2023-01-31 14:41:29 +0200

    Added clay pot

--- Assets/Code/Health system/OnObjectDestruction.cs
+++ Assets/Code/Health system/OnObjectDestruction.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class OnObjectDestruction : AOnDestruction
 {
+    [SerializeField]
+    Animator animator;
+
+    [SerializeField]
+    Collider2D collider;
+
+    protected void Start()
+    {
+        base.Start();
+        animator = GetComponent(typeof(Animator)) as Animator;
+        collider = GetComponent(typeof(Collider2D)) as Collider2D;
+    }
+
     protected override void OnDestruction()
     {
-        Debug.Log("Aboba");
+        if (collider) {
+            collider.enabled = false;
+        }
+        if (animator) {
+            animator.SetTrigger("OnDestruction");
+        }
     }
 }
