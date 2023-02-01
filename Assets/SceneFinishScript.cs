44ed925033353543acb502dfdd4070dfefd41d2e
Artem Teslia <eSquire.github.repos@gmail.com>
2023-02-01 18:03:52 +0200

    Add new level

---
+++ Assets/SceneFinishScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFinishScript : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == GlobalConstants.Tags.PLAYER)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
